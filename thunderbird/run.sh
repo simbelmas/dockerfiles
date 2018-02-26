f00c55ec4dd265bd3a638034f9db56d06a24701e
Simon belmas <22192134+simbelmas@users.noreply.github.com>
2018-02-26 16:07:30 +0100

    thunderbird: don't enforce user

--- thunderbird/run.sh
+++ thunderbird/run.sh
@@ -8,4 +8,4 @@ chown svc: /home/svc -R || true #don't stop if this fail as we can mount ro fs o
 sed "s/USER_ID/${UID}/" -i /home/svc/.config/pulse/client.conf
 
 #run firefox with user
-exec thunderbird --new-instance $@
+su svc -c "exec thunderbird --new-instance $@"

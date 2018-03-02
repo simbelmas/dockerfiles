12bd43b6f93d9530743fd86395b66726c8af2134
Simon belmas <22192134+simbelmas@users.noreply.github.com>
2018-03-02 18:12:44 +0100

    firefox: allow to display local content

--- firefox/run.sh
+++ firefox/run.sh
@@ -3,6 +3,7 @@
 # Ensure permissions are right
 usermod -g ${GID} -u ${UID} svc
 chown svc: /home/svc -R || true #don't stop if this fails as we can mount ro fs on / home
+chown svc: /home/svc/host_pages -R
 
 #Configure pulse client
 sed "s/USER_ID/${UID}/" -i /home/svc/.config/pulse/client.conf

4d3809f22afbf18960d246c99ba706f004415ab3
Simon Belmas <22192134+simbelmas@users.noreply.github.com>
2023-06-16 01:23:08 +0200

    remove transmission

--- transmission/README.md
+++
@@ -1,6 +0,0 @@
-# Transmission
-Transmission rootless container based on alpine.
-
-Volumes:
-/downloads : persistent storage for downloads
-/transmission_config : directory holding settings.json file
--- transmission/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/ash -xe
-#cp -rv /transmission_settings/*.json /config || true
-/usr/bin/transmission-daemon --foreground --no-watch-dir --incomplete-dir=/downloads/incomplete --config-dir=/config --logfile=/dev/stdout --download-dir=/downloads/complete --log-error --log-info --log-debug --pid-file=/tmp/transmission.pid --allowed='*.*.*.*' $@

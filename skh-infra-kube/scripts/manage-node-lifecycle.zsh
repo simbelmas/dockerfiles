1fb618db30335d3aefe4f25cfcf0218cf991b560
Simon Belmas <22192134+simbelmas@users.noreply.github.com>
2025-11-25 15:05:13 +0100

    Node-lifecytcle: add verbose ffor environment variabels management

--- skh-infra-kube/scripts/manage-node-lifecycle.zsh
+++ skh-infra-kube/scripts/manage-node-lifecycle.zsh
@@ -3,21 +3,28 @@ set -e
 
 export $(grep -v '^#' /etc/kubernetes/fedora-kube.d/*.conf | cut -f2- -d':' | xargs)
 
+echo ## Manage Environment variables
 if [[ ! -d "/etc/systemd/system/bootc-fleetlock-fetch-upgrade.service.d" ]] ; then
     mkdir -p /etc/systemd/system/bootc-fleetlock-fetch-upgrade.service.d
 fi
 
 case "${KUBERNETES_NODE_ROLE}" in
     worker)
-        echo -e '[Service]\nEnvironment=NODE_UPGRADE_FLEETLOCK_GROUP=node' > /etc/systemd/system/bootc-fleetlock-fetch-upgrade.service.d/kube-node.conf
+        (
+            set -x
+            echo -e '[Service]\nEnvironment=NODE_UPGRADE_FLEETLOCK_GROUP=node' > /etc/systemd/system/bootc-fleetlock-fetch-upgrade.service.d/kube-node.conf    
+        )
         exec /usr/local/lib/manage_kubernetes_worker_lifecycle.zsh
     ;;
     controlplane)
-        echo -e '[Service]\nEnvironment=NODE_UPGRADE_FLEETLOCK_GROUP=node' > /etc/systemd/system/bootc-fleetlock-fetch-upgrade.service.d/kube-node.conf
+        (
+            set -x
+            echo -e '[Service]\nEnvironment=NODE_UPGRADE_FLEETLOCK_GROUP=node' > /etc/systemd/system/bootc-fleetlock-fetch-upgrade.service.d/kube-node.conf
+        )
         exec /usr/local/lib/manage_kubernetes_controlplane_lifecycle.sh
     ;;
     *)
-    echo "KUBERNETES_NODE_ROLE var has to be woerker|ctplane"
+    echo "KUBERNETES_NODE_ROLE var has to be worker|ctplane"
     exit 1
     ;;
 esac
